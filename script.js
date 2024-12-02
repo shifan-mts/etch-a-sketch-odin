64c596f8ecc4211255871b6e6207c0c40d82d97b
shifan-mts <amohamedshifan1@gmail.com>
2024-12-02 15:17:02 +0530

    final commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const c = document.querySelector(".container");
+let c = document.querySelector(".container");
 c.style = "display:flex; flex-wrap:wrap;";
+for(let j = 1;j<=16;j++){
 for(let i = 1;i<=16;i++){
     let k = document.createElement("div");
     
@@ -7,4 +8,22 @@ for(let i = 1;i<=16;i++){
   
     c.appendChild(k);
 
+} }
+
+function cool(){
+    let n = prompt("enter the number of sides:");
+    c.remove();
+    c = document.createElement("div");
+    c.className = "container";
+    c.style = "display:flex; flex-wrap:wrap;";
+    document.querySelector("body").appendChild(c);
+    for(let i =1;i<=n;i++){
+        for(let j = 1;j<=n;j++){
+            let k = document.createElement("div");
+    
+    k.className = "cool";
+  
+    c.appendChild(k);
+        }
+    }
 }
